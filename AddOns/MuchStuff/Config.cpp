3ad068c4c80b2776333c5792446d2f27f7b6c41f
Streatman <TheStreatman@gmail.com>
2020-12-06 22:44:27 +0100

    1.10.04 (#76)
    * Update playerconstants.c
    
    * Update plantbase.c
    
    * MuchStuff
    
    * Update Msp_AmericanAppleCrate.c
    
    * Update Msp_AppleCrate.c
    
    * Update Cfg.hpp
    
    * Update Config.cpp

---
+++ AddOns/MuchStuff/Config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches
+{
+	class IntenZ_MuchStuff
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = 
+		{
+			"MSP_Storage_crates",
+			"MSP_Scripts",
+			"IntenZ_MissionContainer"
+		};
+	};
+};
+
+class CfgMods
+{
+	class IntenZ_MuchStuff
+	{
+		type = "mod";
+		dependencies[] = {"World"};
+		
+		class defs
+		{
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"IntenZ\MuchStuff\Scripts\4_World"};
+			};
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class Container_Base;
+	class Msp_Kit: Container_Base
+	{
+		itemSize[] = {10,7};
+		weight = 15000;
+	};
+	class WoodenCrate;
+	class Mission_Container: WoodenCrate
+	{
+		model = "\MuchStuffPack\MSP_Storage_crates\militarycrate\militarycrate.p3d";
+	};
+};
