54115d91040ecc367e4a0dd15761d2f841f35725
Landbergaren <landbergaren@gmail.com>
2021-03-13 00:00:29 +0100

    first iterration

---
+++ AddOns/Respawn/Scripts/4_World/playerbase.c
@@ -0,0 +1,14 @@
+modded class PlayerBase {
+    vector previousSpawn;
+    int previousSpawnTimestamp;
+    bool hasBeenKilledByPlayer; 
+
+    override void EEKilled( Object killer ) {
+        super.EEKilled(killer);
+        Man player = Man.Cast(killer);		
+        if (player) {
+            hasBeenKilledByPlayer = true;
+            Print("[Prevent Suicide] Player was killed by another player");
+        }
+    }
+}
---
+++ AddOns/Respawn/Scripts/5_Mission/missionserver.c
@@ -0,0 +1,52 @@
+modded class MissionServer {
+    	PlayerBase OnClientNewEvent(PlayerIdentity identity, vector pos, ParamsReadContext ctx)
+	{
+		string characterType;
+		//m_RespawnMode = GetGame().ServerConfigGetInt("setRespawnMode"); //todo - init somewhere safe
+		SyncRespawnModeInfo(identity);
+		// get login data for new character
+		if ( ProcessLoginData(ctx) && (m_RespawnMode == GameConstants.RESPAWN_MODE_CUSTOM) && !GetGame().GetMenuDefaultCharacterData(false).IsRandomCharacterForced() )
+		{
+			if (GetGame().ListAvailableCharacters().Find(GetGame().GetMenuDefaultCharacterData().GetCharacterType()) > -1)
+				characterType = GetGame().GetMenuDefaultCharacterData().GetCharacterType();
+			else //random type
+				characterType = GetGame().CreateRandomPlayer();
+		}
+		else
+		{
+			characterType = GetGame().CreateRandomPlayer();
+			GetGame().GetMenuDefaultCharacterData().GenerateRandomEquip();
+		}
+        // IntenZ suicide check
+		if (m_player.previousSpawn) {
+            if (!m_player.hasBeenKilledByPlayer && !PlayerLivedLongEnough()) {
+                Print("[Prevent Suicide] player has suicided);
+                Print("[Prevent Suicide] m_player.hasBeenKilledByPlayer inside of OnClinetNewEvent" + m_player.hasBeenKilledByPlayer);
+                Print("[Prevent Suicide] spawwn position changed from " + pos + " to " + m_player.previousSpawn);
+                pos = m_player.previousSpawn;
+            }
+        }
+        m_player.previousSpawnTimestamp = JMDate.GetTimestamp();
+		if (CreateCharacter(identity, pos, ctx, characterType))
+		{
+			EquipCharacter(GetGame().GetMenuDefaultCharacterData());
+		}
+		
+		return m_player;
+	}
+
+    private bool PlayerLivedLongEnough()  
+    {
+        if (!m_player.previousSpawnTimestamp) {
+            return true;
+        }
+        const int minimumTime = 10;
+        int currentDateTime = JMDate.GetTimestamp(); 
+        Print("[Prevent Suicide] currentDateTime inside PlayerLivedLongEnough(): " + currentDateTime);
+        Print("[Prevent Suicide] m_player.previousSpawnTimestamp inside PlayerLivedLongEnough(): " + m_player.previousSpawnTimestamp);
+        int minutesBetweenDeaths = 0;
+        JMDate.TimestampCalculMinutes( m_player.previousSpawnTimestamp, currentDateTime, out int minutesBetweenDeaths);
+        Print("[Prevent Suicide] minutesBetweenDeaths inside PlayerLivedLongEnough() " + minutesBetweenDeaths);
+        return minutesBetweenDeaths > minimumTime;
+    }
+}
